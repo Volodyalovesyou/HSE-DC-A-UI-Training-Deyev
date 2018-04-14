5e58b8dcd6905f4f8d791fa2ef9e991ca738f888
Volodyalovesyou <30952307+Volodyalovesyou@users.noreply.github.com>
2018-04-14 17:08:00 +0300

    accordeon transition

--- javascripts/O_MenuItem.js
+++ javascripts/O_MenuItem.js
@@ -1,12 +1,40 @@
 $(function() {
   $(".A_MenuItemButton").click(function(e) {
-    let container = $(e.target).closest(".O_MenuItem")
+    e.preventDefault()
+
+    let container = $(e.target).closest(".O_MenuItem");
+
+    console.log(container);
+
+    let subItems = container.find(".A_SubmenuItem");
+
+    console.log(subItems);
+
+    let item = subItems[0];
+
+
+
+    let height = 0;
+
+    if (item) {
+      let itemHeight = $(item).height()
+      height = subItems.length * itemHeight
+    };
+
+    container.find(".M_Submenu").height(height);
+
 
     if (container.hasClass("active")) {
       container.removeClass("active");
+
+      container.find(".M_Submenu").height(0);
     } else {
       $(".O_MenuItem").removeClass ("active");
+      $(".M_Submenu").height(0);
+
       container.addClass("active");
+
+      container.find(".M_Submenu").height(height);
     }
   });
 });
